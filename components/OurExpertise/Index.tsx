4ac6557511c83d40bfd3a60207c166ee7882cbde
Todd Owen Mpeli <toddowenmpeli@rocketmail.com>
2025-01-08 12:18:47 +0000

    work.

--- components/OurExpertise/Index.tsx
+++ components/OurExpertise/Index.tsx
@@ -1,22 +1,36 @@
 // Imports
-import { FC } from "react";
-import { IOurExpertise } from "@/components/OurExpertise/types/index";
+import {FC, useRef} from "react";
+import {motion, useScroll, useTransform} from "framer-motion";
+import {IOurExpertise} from "@/components/OurExpertise/types/index";
 
 // Styling
 import styles from "@/components/OurExpertise/styles/OurExpertise.module.scss";
 
 // Components
+import TextRevealBlurEffect from "@/components/Hero/Fragments/TextRevealBlurEffect";
 
-const OurExpertise:FC<IOurExpertise.IProps> = ({
-	title,
-	paragraph,
-}) => {
-    return (
-        <div className={styles.ourExpertise}>
-           <div className={styles.container}>
+const OurExpertise: FC<IOurExpertise.IProps> = ({title, paragraph}) => {
+	// Track the progress of the scroll and scale
+	const container = useRef(null);
+	const {scrollY} = useScroll();
+	// Scroll Opacity Div
+	const scrollOpacity = useTransform(scrollY, [0, 25], [1, 0]);
+
+	return (
+		<motion.div ref={container} className={styles.ourExpertise}>
+			<div className={styles.container}>
+				<div className={styles.top}>
+					<motion.h2 className={styles.title}>{title}</motion.h2>
+					<TextRevealBlurEffect
+						content={paragraph}
+						scrollOpacity={scrollOpacity}
+						className={paragraph ? styles.paragraph : "hidden"}
+					/>
+				</div>
+				<div className={styles.main}></div>
 			</div>
-        </div>
-    );
-}
+		</motion.div>
+	);
+};
 
 export default OurExpertise;
