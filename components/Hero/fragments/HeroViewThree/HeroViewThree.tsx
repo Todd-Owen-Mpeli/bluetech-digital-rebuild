a3aa9a3080fcea0e6e2c8f370b47643e5f081ae7
Todd Mpeli <toddowenmpeli@rocketmail.com>
2025-06-02 22:21:48 +0300

    Late night work.

--- components/Hero/fragments/HeroViewThree/HeroViewThree.tsx
+++ components/Hero/fragments/HeroViewThree/HeroViewThree.tsx
@@ -1,15 +1,22 @@
 // Imports
+import {
+	fadeIn,
+	initialTwo,
+	slideInLeftInitial,
+	slideInRightFinish,
+	slideInRightInitial
+} from "@/animations/animations";
 import { FC } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import {IHero} from "@/components/Hero/types/index";
-import { fadeIn, initialTwo, slideInLeftInitial, slideInRightFinish } from "@/animations/animations";
 
 // Styling
 import styles from "@/components/Hero/styles/Hero.module.scss";
 
 // Components
 import Button from "@/components/Elements/Button/Button";
+import Paragraph from "@/components/Elements/Paragraph/Paragraph";
 import VideoCard from "@/components/Hero/fragments/HeroViewThree/Elements/VideoCard";
 import SlideUpDivMaskReveal from "@/components/Animations/SlideUpDivMaskReveal/SlideUpDivMaskReveal";
 import ContentSliceRevealMaskAnimation from "@/components/Animations/ContentSliceRevealMaskAnimation";
@@ -22,47 +29,76 @@ const HeroViewThree: FC<IHero.IHeroViewThree.IProps> = ({
     backgroundImage
  }) => {
     return (
-		<div className={`${styles.heroViewThree} panel`}>
-			<Link
-				href={`${link?.url || "/"}`}
-				aria-label={`${link?.title}`}
-				className={styles.linkWrapper}
-				target={link?.target || "_self"}
-			>
-				<SlideUpDivMaskReveal
-					triggerOnce={true}
-					className={styles.mainContainer}
-					backgroundColor={"bg-pureBlack"}
-					style={{
-						backgroundImage: `url("${backgroundImage?.sourceUrl}")`,
-					}}
+		<>
+			<div className={`${styles.heroViewThree} panel`}>
+				<Link
+					href={`${link?.url || "/"}`}
+					aria-label={`${link?.title}`}
+					className={styles.linkWrapper}
+					target={link?.target || "_self"}
 				>
-					<VideoCard video={video} displayVideo={displayVideo}/>
-					<div className={styles.contentWrapper}>
-					<motion.div
-						viewport={{once: false}}
-						className={styles.content}
-						initial={slideInLeftInitial}
-						whileInView={slideInRightFinish}
+					<SlideUpDivMaskReveal
+						triggerOnce={true}
+						className={styles.mainContainer}
+						backgroundColor={"bg-pureBlack"}
+						style={{
+							backgroundImage: `url("${backgroundImage?.sourceUrl}")`,
+						}}
 					>
-						<ContentSliceRevealMaskAnimation>
-							<motion.h4
+						<VideoCard video={video} displayVideo={displayVideo}/>
+						<div className={styles.contentWrapper}>
+						<motion.div
+							viewport={{once: false}}
+							className={styles.content}
+							initial={slideInLeftInitial}
+							whileInView={slideInRightFinish}
+						>
+							<ContentSliceRevealMaskAnimation>
+								<motion.h4
 									initial={initialTwo}
 									whileInView={fadeIn}
 									viewport={{once: true}}
 									className={styles.title}
 								>
 									{title}
-							</motion.h4>
-						</ContentSliceRevealMaskAnimation>
-						<div className={styles.playButton}>
-							<Button styleNumber={0} link={link}/>
+								</motion.h4>
+							</ContentSliceRevealMaskAnimation>
+							<div className={styles.playButton}>
+								<Button styleNumber={0} link={link}/>
+							</div>
+						</motion.div>
 						</div>
+					</SlideUpDivMaskReveal>
+				</Link>
+			</div>
+			<div className={`${styles.heroViewFour} panel`}>
+				{/* <WaterDistortionEffect
+					image={backgroundImage}
+					className={styles.contentWrapper}
+				>
+					<ContentSliceRevealMaskAnimation>
+						<motion.h4
+							initial={initialTwo}
+							whileInView={fadeIn}
+							viewport={{once: true}}
+							className={styles.title}
+						>
+							{title}
+						</motion.h4>
+					</ContentSliceRevealMaskAnimation>
+					<motion.div
+                        viewport={{once: false}}
+                        initial={slideInLeftInitial}
+                        whileInView={slideInRightFinish}
+					>
+						<Paragraph
+							className={styles.paragraph}
+							content={"Your UI stays interactive above the OGL canvas."}
+						/>
 					</motion.div>
-					</div>
-				</SlideUpDivMaskReveal>
-			</Link>
-		</div>
+				</WaterDistortionEffect> */}
+			</div>
+		</>
     );
 }
 
