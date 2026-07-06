237a6d4ffa620b8d4f1b1d8a3ded0868ba62ae5d
Todd <toddowenmpeli02@gmail.com>
2026-07-06 10:36:20 +0300

    Updated Npm Packages.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import {IGlobal} from "@/context/types/context";
 // TypeScript may complain about side-effect only import of SCSS modules
 // when no declaration file exists. Silence the error for this import.
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
 import "@/styles/globals.scss";
 
 // Queries Functions
